3c4a9eea79648425f537d788e2992fcefbd772c8
Christofer Flores <cristofer.flort@gmail.com>
2019-05-28 13:23:04 -0500

    Create the events app along with the migrations

---
+++ admin/events/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    name = models.TextField()
+    is_active = models.BooleanField(default=False)
+    level = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    name = models.TextField()
+    value = models.TextField()
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
