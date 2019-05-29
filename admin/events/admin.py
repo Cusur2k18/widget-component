a750eea4e62d3466e9f1349378917c590f350c2c
Christofer Flores <cristofer.flort@gmail.com>
2019-05-28 21:42:39 -0500

    Configure the index and the form view of the events

--- admin/events/admin.py
+++ admin/events/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 from .models import Event, Measurement
 
-admin.site.register(Event)
-admin.site.register(Measurement)
+
+class EventAdmin(admin.ModelAdmin):    
+    # Form view
+    fields = ['is_active', 'name', 'level']
+
+    # Index view
+    list_display = ('_name', '_is_active', '_level', '_updated_at')
+
 
 # Register your models here.
+admin.site.register(Event, EventAdmin)
+admin.site.register(Measurement)
--- admin/events/models.py
+++ admin/events/models.py
@@ -9,6 +9,27 @@ class Event(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def _name(self):
+        return self.name
+    _name.short_description = 'Nombre'
+
+    def _is_active(self):
+        return self.is_active
+    _is_active.short_description = 'Activo'
+    _is_active.boolean = True
+
+    def _level(self):
+        return self.level
+    _level.short_description = 'Nivel'
+
+    def _created_at(self):
+        return self.created_at
+    _created_at.short_description = 'Fecha creación'
+
+    def _updated_at(self):
+        return self.updated_at
+    _updated_at.short_description = 'Ultima actualización'
+
     def __str__(self):
         return self.name
 
