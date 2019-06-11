5c9b02fbcb8b0f81eb01740aee0990a72b03bba8
Christofer Flores <cristofer.flort@gmail.com>
2019-06-11 10:30:49 -0500

    Prepare everything for deploy to heroku

---
+++ admin/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn widget_admin.wsgi
--- admin/widget/views.py
+++ admin/widget/views.py
@@ -5,19 +5,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.forms.models import model_to_dict
 from .models import Agent
 
-# Create your views here.
-def index(request):
-	try:
-		agents = Agent.objects.filter(is_active=True)
-
-		if (not agents):
-			raise ObjectDoesNotExist
-		
-		metrics = list(agents.first().metric_set.all().values())
-		return JsonResponse({"agent": list(agents.values())[0], "metrics": metrics})
-
-	except ObjectDoesNotExist:
-		return JsonResponse({"error":"no metrics"})
 
 def widget(request):
 	agents = Agent.objects.prefetch_related('metric_set', 'widget').filter(is_active=True)
@@ -29,15 +16,3 @@ def widget(request):
 	}
 	# import pdb; pdb.set_trace()
 	return render(request, 'widget/agent.html', { "agent": agent, "colors": colors, "total": range(1, agent.widget.total_level + 1) })
-
-	# try:
-	# 	agents = Agent.objects.filter(is_active=True)
-
-	# 	if (not agents):
-	# 		raise ObjectDoesNotExist
-		
-	# 	metrics = list(agents.first().metric_set.all().values())
-	# 	return JsonResponse({"agent": list(agents.values())[0], "metrics": metrics})
-
-	# except ObjectDoesNotExist:
-	# 	return JsonResponse({"error":"no metrics"})
